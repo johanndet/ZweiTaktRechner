29ea3fcb77d21b86c61db3f1d9d19eaf2875422d
johanndet <johanndet@web.de>
2024-10-25 21:37:18 +0200

    first version

---
+++ myframe.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <wx/wx.h>
+
+enum Id{
+    ID_BUTTON,
+    ID_TEXT_BENZIN,
+    ID_TEXT_OEL,
+    ID_COMBO_BOX
+};
+
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame();
+private: 
+  wxButton *butt;
+  wxTextCtrl *text_benzin;
+  wxTextCtrl *text_oel;
+  wxComboBox *combo_box;
+  wxStaticText *label;
+  void button_calculate(wxCommandEvent& event);
+};
+
